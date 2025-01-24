8744169422020e9f4e4bc7a23c4ca55c1ca238aa
kartrr <162323158+kartrr@users.noreply.github.com>
2025-01-24 18:10:42 +0100

    Upload test2

---
+++ tests/load_test2.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+// Настраиваем сценарий
+export let options = {
+  stages: [
+    { duration: '10s', target: 1 }, // 10 пользователей в течение первых 30 секунд
+    { duration: '20s', target: 2 }, // Увеличение до 50 пользователей за 1 минуту
+    { duration: '20s', target: 3 }, // Поддерживаем нагрузку с 50 пользователями 2 минуты
+    { duration: '10s', target: 0 }, // Уменьшение нагрузки до 0 за 30 секунд
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // 95% запросов должны завершаться за 500 мс
+  },
+};
+
+// Основная функция, выполняемая каждым виртуальным пользователем
+export default function () {
+  const res = http.get('https://test.k6.io/);
+  if (res.status !== 200) {
+    console.error(`Request failed. Status: ${res.status}`);
+  }
+  sleep(1); // Имитация паузы между запросами
+}
