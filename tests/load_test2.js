248e71f2be3524c0c74d68097473119c06d88430
Aleksandr Kolesnikov <162323158+kartrr@users.noreply.github.com>
2025-02-03 13:08:02 +0100

    Update load_test2.js

--- tests/load_test2.js
+++ tests/load_test2.js
@@ -1,24 +1,24 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-// Настраиваем сценарий
+// setup
 export let options = {
   stages: [
-    { duration: '10s', target: 1 }, // 10 пользователей в течение первых 30 секунд
-    { duration: '20s', target: 2 }, // Увеличение до 50 пользователей за 1 минуту
-    { duration: '20s', target: 3 }, // Поддерживаем нагрузку с 50 пользователями 2 минуты
-    { duration: '10s', target: 0 }, // Уменьшение нагрузки до 0 за 30 секунд
+    { duration: '10s', target: 1 }, // 1 user per 30 10 sec
+    { duration: '20s', target: 2 }, // 2u per 20s
+    { duration: '20s', target: 3 }, // 3u per 20s 
+    { duration: '10s', target: 0 }, // 0u per 10s
   ],
   thresholds: {
-    http_req_duration: ['p(95)<500'], // 95% запросов должны завершаться за 500 мс
+    http_req_duration: ['p(95)<500'], // 95% requests in 500 ms
   },
 };
 
-// Основная функция, выполняемая каждым виртуальным пользователем
+// main func for every user
 export default function () {
   const res = http.get('https://test.k6.io');
   if (res.status !== 200) {
     console.error(`Request failed. Status: ${res.status}`);
   }
-  sleep(1); // Имитация паузы между запросами
+  sleep(1); // pause
 }
